96eead48f2ce3a5055dc60ffe1d4c44b7938abd9
ann <spieglovaa.07@spst.eu>
2026-04-17 11:28:03 +0200

    Add files via upload

---
+++ testyRPR/ClenCechu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testyRPR
+{
+    public class ClenCechu
+    {
+        public string jmeno { get; set; }
+        public int energie { get; }
+
+        public void Trenuj(int pocet) { 
+        //:p
+        }
+
+        public void UtrzZraneni(int dmg) { 
+        //c:
+        }
+    }
+}
